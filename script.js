7cbc5739da5b58ad25d12eec56d38f1c194ba054
victoria <vikabirukova22@gmail.com>
2023-09-07 23:01:08 +0300

    commit without descriptions

--- script.js
+++ script.js
@@ -26,18 +26,15 @@ console.log(map.values());
 map.set(3, 'third');
 
 function getNumeral (text) {
-    const words = text.split(' ');  //створюється масив зі слів
+    const words = text.split(' ');
     const replacedNumbersArray = words.map(word => {
-        // методом map змінюємо масив words, створюючи новий масив зі зміненими елементами
         const num = Number(word);
-        //перетворюємо рядок в число
-        if (map.has(num)) { //перевіряємо чи є отримане в num значення ключем карти map
-            return map.get(num); //якщо так, повертаємо значення ключа num
+        if (map.has(num)) {
+            return map.get(num); 
         }
         return word;
     });
     return replacedNumbersArray.join (' '); 
-    //отриманий масив із заміненими числами перетворюємо назад в єдиний рядок за допомоою методу join і повертаємо як результат функції
 };
 
 const text = 'This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents.';
